34bbc9994f08a03945574312b8ccae8302edbdc9
Cole Robinson <elocnosnibor@gmail.com>
2016-10-31 12:17:24 -0700

    cleaned out comments

--- client/factories/dummyFactory.js
+++ client/factories/dummyFactory.js
@@ -1,11 +1,5 @@
 myApp.factory('dummyFactory', function($http){
 
-	// This is my dummyFactory. I usually add this into any project that 
-	// I do. Just so that I can use it for reference as I add new Factories
-	// below we have an example of how we would create a post request, as well 
-	// as how we would create a get request. 
-
-
 	var dummies = []
 
 	var factory = {}
@@ -17,14 +11,8 @@ myApp.factory('dummyFactory', function($http){
 		});
 	}
 
-	// the info parameter below is the the dummy that we're trying to add into our database
-
-	// I added a test for myself below. I'm passing both a body element as well a params element
-	// I use this as an initial test that I can pass information to my backend.
-	// Check out your server console and you should see the body and the value we pass through 
-	// the url. 
 	factory.addDummy = function(info, callback){
-		$http.post('/dummies/youShouldSeeThisInServerConsoleReqParams', info).then(function(data){
+		$http.post('/dummies', info).then(function(data){
 			if(data.error){
 				callback(data);
 			} else {
--- server/models/mongoose.js
+++ server/models/mongoose.js
@@ -7,6 +7,6 @@ var MongooseSchema = new mongoose.Schema({
 
 mongoose.model('Mongoosedb', MongooseSchema);
 // Validations
-MongooseSchema.path('color').required(true, 'Color cannot be blank');
-MongooseSchema.path('weight').required(true, 'Weight cannot be blank');
-MongooseSchema.path('name').required(true, 'Name cannot be blank');
+// MongooseSchema.path('color').required(true, 'Color cannot be blank');
+// MongooseSchema.path('weight').required(true, 'Weight cannot be blank');
+// MongooseSchema.path('name').required(true, 'Name cannot be blank');
